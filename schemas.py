1b893a68587774c60af2ec46eb2ba84a52e68ec2
VLEAIT <133334975+VLEAIT@users.noreply.github.com>
2026-05-07 13:49:31 +0000

    user class entry with validation

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel,Field
-from typing import Optional
+from pydantic import BaseModel,Field,EmailStr,model_validator
+from typing import Optional,literal
 from fastapi import FastAPI
 
 class BookCreate(BaseModel):
@@ -20,3 +20,54 @@ class BookUpdate(BaseModel):
     price:Optional[float]=Field(None,ge=0)
     description=Optional[str]=None
 
+
+class UserBase(BaseModel):
+    id:int
+    username:str=Field(min_length=6,max_length=20)
+    full_name:str
+    is_active:bool=Field(default=True)
+    email:EmailStr
+
+    @field_validator("username")
+    @classmethod
+    def validate_username(cls,v:str)->str:
+        v=v.strip()
+        if " " in v:
+            raise ValueError("no space allowed")
+        return v    
+
+class UserCreate(UserBase):
+    password:str=Field(min_length=8,max_length=20)
+    confirm_password:str=Field(min_length=8,max_length=20)
+
+    @model_validator(mode="after")
+    def hashed_password(self):
+        if self.password!=self.confirm_password:
+            raise ValueError("Password no conform")
+            return self
+
+
+class UserLogin(BaseModel):
+    email:EmailStr
+    password:str
+
+class UserUpdate(BaseModel):
+    username=Optional[str]=Field(None,min_length=6,max_length=20)
+    full_name=Optional[str]=None
+    is_active=Optional[bool]=None
+    email=Optional[EmailStr]=None
+
+    @field_validator("username")
+    @classmethod
+    def verify_username(cls,v:str)->str:
+        v=v.strip()
+        if " " in v:
+            raise ValueError("no space allowed") 
+        return v    
+
+class UserPasswordUpdate(BaseModel):
+    old_password:
+ 
+    
+
+    
