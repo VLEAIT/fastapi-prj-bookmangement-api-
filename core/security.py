39b4b8b9e9adb32e7c06b988caf76467d4e023be
VLEAIT <133334975+VLEAIT@users.noreply.github.com>
2026-05-28 14:18:12 +0000

    password hashing

--- core/security.py
+++ core/security.py
@@ -1,10 +1,14 @@
 from passlib.context import CryptoContext
-from datetime import datetime,timedelta,timezone
-from jose import jwt,JWTError
-import os
+from datetime import timedelta,datetime,timezone
+from json import JWTError,jwt
 from typing import Optional
+import os
 
-pwd=CryptoContext(schemes=["bcrypt"],deprecated="auto",bcrypt_handle_long_passwords=True)
+pwd_context=CryptoContext(
+    schemes=["bcrypt"],
+    deprecated="auto",
+    bcrypt_handle_long_password=True
+)
 
 SECRET_KEY=os.getenv("SECRET_KEY")
 ALGORITHM="HS256"
@@ -12,22 +16,24 @@ TOKEN_EXPIRE_MINUTES=30
 
 
 def hashed_password(password:str)->str:
-    return pwd.hash(password)
+    return pwd_context.hash(password)
 
-def verifY_passport(hashed:str,plain:str)->bool:
-    return pwd.verify(plain,hashed)
 
-def create_acess_token(data:dict)->str:
+def verify_password(plain:str,hashed:str)->bool:
+    return pwd_context.verify(plain,hashed)
+
+def create_token(data:dict)->str:
     to_encode=data.copy()
-    expire=datetime.now(timezone.utc) + timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+    expire=datetime.now(timezone.utc)+ timedelta(minutes=TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp":expire})
     return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
 
-def decode_token(token:str)->Optional[dict]:
+def decode(token:str)->Optional[dict]:
     try:
         return jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
     except JWTError:
         return None    
+
 
 
 
