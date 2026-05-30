fd76c0a65bd4682410c84d0a771006596ba9b636
VLEAIT <133334975+VLEAIT@users.noreply.github.com>
2026-05-30 02:23:19 +0000

    login route

--- core/config.py
+++ core/config.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException,APIRouter,Depends
 from database import get_db
 from schemas import UserCreate,UserResponse,
 from models import User
-from core import hashed_password,verify_password 
+from core import hashed_password,verify_password,create_token
 
 
 
@@ -16,13 +16,20 @@ router=APIRouter(
 def register(user:UserCreate,db:Sessions=Depends(get_db)):
     existing=db.query(User).filter(User.email==user.email).first()
     if existing:
-        HTTPException(status_code=status.HTTP_204_NO_CONTENT,detail="no content")
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT,detail="no content")
     hashed=hashed_password(password)
     db_user=User(**user.model_dump(exclude:{"password","conform_password"}),password=hashed)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
-    
-        
-      
+
+#login
+@router.post("/login")
+def login(user:UserLogin,db:Sessions=Depends(get_db)):
+    existing =db.query(User).filter(User.email==user.email).first()
+    if not existing or not verify_password(user.password,existing.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="the value is not authorized")
+    token=create_token({"sub":str(existing.id)})
+    return {"acces_token":token,"token_type":"bearer"}    
+
