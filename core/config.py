ade777cce3a5411c600ad08faa2d97d2bf757f23
VLEAIT <133334975+VLEAIT@users.noreply.github.com>
2026-05-30 02:12:27 +0000

    register route

---
+++ core/config.py
@@ -0,0 +1,28 @@
+from fastapi import HTTPException,APIRouter,Depends
+from database import get_db
+from schemas import UserCreate,UserResponse,
+from models import User
+from core import hashed_password,verify_password 
+
+
+
+
+router=APIRouter(
+    preflix="/auth",
+    tags=["auth"]
+)
+#Register
+@router.post("/register",response_model=UserResponse,status_code=201)
+def register(user:UserCreate,db:Sessions=Depends(get_db)):
+    existing=db.query(User).filter(User.email==user.email).first()
+    if existing:
+        HTTPException(status_code=status.HTTP_204_NO_CONTENT,detail="no content")
+    hashed=hashed_password(password)
+    db_user=User(**user.model_dump(exclude:{"password","conform_password"}),password=hashed)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+    
+        
+      
